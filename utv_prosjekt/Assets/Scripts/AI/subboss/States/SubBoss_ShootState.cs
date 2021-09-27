d38b6e5244c811ca98db91205e802800bcc97c9a
3ldins <91253537+3ldins@users.noreply.github.com>
2021-09-27 13:22:31 +0200

    scholpcya

---
+++ utv_prosjekt/Assets/Scripts/AI/subboss/States/SubBoss_ShootState.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubBoss_ShootState : StateDIA
+{
+    // Runs on initialization
+    public SubBoss_ShootState(object mainScript) : base(mainScript)
+    {
+
+    }
+
+    // Runs once before the first OnUpdate() when state is activated
+    public override void OnEnter(dynamic[] args)
+    {
+
+    }
+
+    // Runs every frame the state is active
+    public override void OnUpdate()
+    {
+
+    }
+
+    // Runs when the state stops
+    public override void OnExit()
+    {
+
+    }
+}
---
+++ utv_prosjekt/Assets/Scripts/player/playershootin.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playershootin : MonoBehaviour
+{
+
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] float bulletSpeed = 15;
+    [SerializeField] float bulletFiringPeriod = 0.1f;
+
+    Coroutine fireCoroutine;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Shoot();
+    }
+
+    private void Shoot()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            fireCoroutine = StartCoroutine(FireContinously());
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            StopCoroutine(fireCoroutine);
+        }
+    }
+
+    private IEnumerator FireContinously()
+    {
+        while (true)
+        {
+            var bulletPos = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z + 1);
+            GameObject bullet = Instantiate(bulletPrefab, bulletPos, Quaternion.identity);
+            bullet.GetComponent<Rigidbody2D>().velocity = new Vector2(0, bulletSpeed);
+            yield return new WaitForSeconds(bulletFiringPeriod);
+        }
+    }
+
+
+}
